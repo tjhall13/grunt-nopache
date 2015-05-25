d9996ae32434ce4b7210efab84b7b5a8ab2fd230
thall3 <thall3@unl.edu>
2015-05-25 03:44:30 -0500

    Initial Commit

---
+++ tasks/nopache.js
@@ -0,0 +1,35 @@
+/*
+ * grunt-nopache
+ * https://github.com/tjhall13/grunt-nopache
+ *
+ * Copyright (c) 2015 Trevor Hall
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var Nopache = require('nopache').nopache;
+
+module.exports = function(grunt) {
+    
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('nopache', 'Grunt nopache task.', function() {
+    var done = this.async();
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      port: 10000,
+      base: '.',
+      keepAlive: false
+    });
+    
+    var server = new Nopache(options.base, options.port);
+    
+    server.listen();
+    
+    if(!options.keepAlive) {
+        done();
+    }
+  });
+};
